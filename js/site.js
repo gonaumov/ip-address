306ca1299e1e177390c16356a7c397e9dc2b169b
Georgi Naumov <gonaumov@gmail.com>
2015-01-03 19:13:48 +0200

    Initial commit.

---
+++ js/site.js
@@ -0,0 +1,20 @@
+﻿// Load jQuery
+google.load("jquery", "1.3.2");
+// on page load complete
+google.setOnLoadCallback(function() {
+	var _licosURL = "http://hb.lycos.com/hb.js";
+	var _ipAddRess = null;
+	$.getScript(_licosURL,
+	function()
+	{
+		$('#info_holder').html("");
+		$('<table id="ipinfo_table"></table>').appendTo("#info_holder");
+		if(typeof GeoIP != "undefined")
+		{
+			for(i in GeoIP)
+			{
+				$('<tr><td>'+i+'</td><td>'+GeoIP[i]+'</td></tr>').appendTo("#ipinfo_table");
+			}
+		}
+	});
+});
